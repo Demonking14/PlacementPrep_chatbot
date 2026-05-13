b2c6729fde58191887016f3378a93d488b655244
Demonking14 <adi.jha1424@gmail.com>
2026-05-13 18:55:34 +0530

    Implement thread management and message loading in frontend

--- frontend.py
+++ frontend.py
@@ -1,15 +1,61 @@
 from backend import workflow
 from langchain_core.messages import HumanMessage
 import streamlit as st
+import uuid
 
-CONFIG = {"configurable" : {"thread_id" : "thread1"}}
+def generateThread():
+    thread_id = uuid.uuid4()
+    return thread_id;
+
+def resetMesssage():
+     thread_id = generateThread()
+     st.session_state['thread_id']=thread_id
+     add_threads(threadid=thread_id);
+     st.session_state['msg_history']  = []
+     
+def add_threads(threadid):
+    if threadid not in st.session_state['chat_thread']:
+        st.session_state['chat_thread'].append(threadid);     
+     
+def loadChats(threadid):
+    return workflow.get_state(config={"configurable":{"thread_id": threadid}}).values.get('message', [])
+
+
+
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id'] = generateThread()
+
+if 'chat_thread' not in st.session_state:
+    st.session_state['chat_thread'] = []
 
 if 'msg_history' not in st.session_state:
-    st.session_state['msg_history'] = [];
-else: 
-    for message in st.session_state['msg_history']:
-        with st.chat_message(message['role']):
-            st.text(message['message'])
+    st.session_state['msg_history'] = []
+
+CONFIG = {"configurable" : {"thread_id" : st.session_state['thread_id']}}
+add_threads(st.session_state['thread_id'])
+
+st.sidebar.header("My Conversations")
+if st.sidebar.button("New Chat"):
+    resetMesssage()
+ 
+
+st.sidebar.header("Message history")
+for thread_id in st.session_state['chat_thread'][::]:
+    if st.sidebar.button(str(thread_id)):
+        st.session_state['thread_id'] = thread_id
+        messages = loadChats(threadid=thread_id)
+        temp_msg = []
+        for msg in messages:
+            if isinstance(msg, HumanMessage):
+                role = 'user'
+            else:
+                role = 'ai'
+            temp_msg.append({'role': role, 'message': msg.content})
+        st.session_state['msg_history'] = temp_msg
+
+for message in st.session_state['msg_history']:
+    with st.chat_message(message['role']):
+        st.text(message['message'])
 
 user_input = st.chat_input("Type your queries here")
 
